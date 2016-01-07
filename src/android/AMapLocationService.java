fa0596cca5e3323976beefdee05793083ccd577c
csbz17027 <77113015@qq.com>
2016-01-07 17:24:27 +0800

    Update amap version to 2.2

--- src/android/AMapLocationService.java
+++ src/android/AMapLocationService.java
@@ -9,9 +9,10 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationClientOption.AMapLocationMode;
 import com.amap.api.location.AMapLocationListener;
-import com.amap.api.location.LocationManagerProxy;
-import com.amap.api.location.LocationProviderProxy;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
@@ -23,80 +24,144 @@ public class AMapLocationService extends CordovaPlugin implements AMapLocationLi
 
     private static final String STOP_ACTION = "stop";
     private static final String GET_ACTION = "getCurrentPosition";
-    CallbackContext callbackContext;
-    LocationManagerProxy mLocationManagerProxy;
+    private static final String WATCH_ACTION = "watch";
+    private static final String CHANGE_OPTION_ACTION = "changeOption";
+    CallbackContext cbCtx;
     JSONObject jsonObj = new JSONObject();
+    AMapLocationClientOption mLocationOption = null;
+    AMapLocationClient mLocationClient = null;
 
     @Override
-    public boolean execute(String action, JSONArray args,
+    public boolean execute(final String action, final JSONArray args,
                            final CallbackContext callbackContext) {
         setCallbackContext(callbackContext);
         if (GET_ACTION.equals(action)) {
             PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
             pluginResult.setKeepCallback(true);
-            this.callbackContext.sendPluginResult(pluginResult);
-            if (mLocationManagerProxy == null) {
+            cbCtx.sendPluginResult(pluginResult);
+            if (mLocationClient == null) {
                 cordova.getThreadPool().execute(new Runnable() {
                     public void run() {
-                        mLocationManagerProxy = LocationManagerProxy.getInstance(cordova.getActivity());
-                        mLocationManagerProxy.requestLocationData(LocationProviderProxy.AMapNetwork, 10 * 1000, 10, AMapLocationService.this);
+                        mLocationOption = new AMapLocationClientOption();
+                        mLocationOption.setLocationMode(AMapLocationMode.Hight_Accuracy);
+                        mLocationOption.setNeedAddress(true);
+                        mLocationOption.setOnceLocation(false);
+                        mLocationOption.setWifiActiveScan(true);
+                        mLocationOption.setMockEnable(false);
+                        mLocationOption.setInterval(10 * 1000);
+                        mLocationClient = new AMapLocationClient(cordova.getActivity());
+                        mLocationClient.setLocationOption(mLocationOption);
+                        mLocationClient.setLocationListener(AMapLocationService.this);
+                        mLocationClient.startLocation();
                     }
                 });
             }
-//            else {
-//                try {
-//                    if (jsonObj.getInt("errCode") == 0) {
-//                        callbackContext.success(jsonObj);
-//                    } else {
-//                        callbackContext.error(jsonObj);
-//                    }
-//                } catch (JSONException e) {
-//                    e.printStackTrace();
-//                }
-//
-//            }
             return true;
         }
+        if (WATCH_ACTION.equals(action)) {
+            PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
+            pluginResult.setKeepCallback(true);
+            cbCtx.sendPluginResult(pluginResult);
+            if (mLocationClient == null) {
+                cordova.getThreadPool().execute(new Runnable() {
+                    public void run() {
+                        setLocationOption(args);
+                        mLocationClient = new AMapLocationClient(cordova.getActivity());
+                        mLocationClient.setLocationOption(mLocationOption);
+                        mLocationClient.setLocationListener(AMapLocationService.this);
+                        mLocationClient.startLocation();
+                    }
+                });
+            }
+            return true;
+        }
+        if (CHANGE_OPTION_ACTION.equals(action)) {
+            setLocationOption(args);
+            if (mLocationClient == null) {
+                mLocationClient = new AMapLocationClient(cordova.getActivity());
+                mLocationClient.setLocationListener(this);
+            }
+            mLocationClient.setLocationOption(mLocationOption);
+            return true;
+        }
+
         if (STOP_ACTION.equals(action)) {
             stopLocation();
-            callbackContext.success(200);
+            cbCtx.success(200);
             PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, 200);
-            callbackContext.sendPluginResult(pluginResult);
             pluginResult.setKeepCallback(false);
+            cbCtx.sendPluginResult(pluginResult);
             return true;
         }
-        PluginResult pluginResult = new PluginResult(PluginResult.Status.ERROR,PluginResult.Status.INVALID_ACTION.toString());
-        callbackContext.sendPluginResult(pluginResult);
+        PluginResult pluginResult = new PluginResult(PluginResult.Status.ERROR, PluginResult.Status.INVALID_ACTION.toString());
         pluginResult.setKeepCallback(false);
+        cbCtx.sendPluginResult(pluginResult);
         return false;
     }
 
-    private void stopLocation() {
-        if (mLocationManagerProxy != null) {
-            mLocationManagerProxy.removeUpdates(this);
-            mLocationManagerProxy.destory();
+    private void setLocationOption(JSONArray args) {
+        try {
+            if (mLocationOption == null) {
+                mLocationOption = new AMapLocationClientOption();
+            }
+            mLocationOption.setInterval(args.getInt(0) * 1000);
+            mLocationOption.setNeedAddress(args.getBoolean(1));
+            mLocationOption.setOnceLocation(args.getBoolean(2));
+            mLocationOption.setGpsFirst(args.getBoolean(3));
+            mLocationOption.setHttpTimeOut(args.getInt(4) * 1000);
+            mLocationOption.setWifiActiveScan(args.getBoolean(5));
+            int locationType = args.getInt(6);
+            switch (locationType) {
+                case 0:
+                    mLocationOption.setLocationMode(AMapLocationMode.Hight_Accuracy);
+                    break;
+                case 1:
+                    mLocationOption.setLocationMode(AMapLocationMode.Device_Sensors);
+                    break;
+                case 2:
+                    mLocationOption.setLocationMode(AMapLocationMode.Battery_Saving);
+                    break;
+                default:
+                    mLocationOption.setLocationMode(AMapLocationMode.Hight_Accuracy);
+                    break;
+            }
+            mLocationOption.setLocationMode(AMapLocationMode.Hight_Accuracy);
+            mLocationOption.setMockEnable(false);
+        } catch (JSONException e) {
+            PluginResult pluginResult = new PluginResult(PluginResult.Status.ERROR, "json解析错误，请确认参数是否有误");
+            pluginResult.setKeepCallback(false);
+            cbCtx.sendPluginResult(pluginResult);
         }
-        mLocationManagerProxy = null;
     }
+
+    private void stopLocation() {
+        if (mLocationClient != null) {
+            mLocationClient.onDestroy();
+            mLocationClient = null;
+            mLocationOption = null;
+        }
+    }
+
 
     @Override
     public void onDestroy() {
-        stopLocation();
         super.onDestroy();
+        stopLocation();
     }
 
     public CallbackContext getCallbackContext() {
-        return callbackContext;
+        return cbCtx;
     }
 
     public void setCallbackContext(CallbackContext callbackContext) {
-        this.callbackContext = callbackContext;
+        cbCtx = callbackContext;
     }
 
     @Override
     public void onLocationChanged(AMapLocation aMapLocation) {
         try {
-            if (aMapLocation != null && aMapLocation.getAMapException().getErrorCode() == 0) {
+            if (aMapLocation != null && aMapLocation.getErrorCode() == 0) {
+                Log.d("aMapLocation", aMapLocation.toString());
                 //获取位置信息
                 Double geoLat = aMapLocation.getLatitude();
                 Double geoLng = aMapLocation.getLongitude();
@@ -104,50 +169,41 @@ public class AMapLocationService extends CordovaPlugin implements AMapLocationLi
                 JSONObject position = new JSONObject();
                 position.put("lat", aMapLocation.getLatitude());
                 position.put("lng", aMapLocation.getLongitude());
-                jsonObj.put("errCode", aMapLocation.getAMapException().getErrorCode());
+                jsonObj.put("errCode", aMapLocation.getErrorCode());
                 jsonObj.put("position", position);
-                jsonObj.put("accuracy", aMapLocation.getAccuracy());
-                jsonObj.put("provider", aMapLocation.getProvider());
                 jsonObj.put("address", aMapLocation.getAddress());
-                jsonObj.put("time", aMapLocation.getTime());
-                Log.d("lanceLocation", jsonObj.toString());
+                jsonObj.put("adCode", aMapLocation.getAdCode());
+                jsonObj.put("province", aMapLocation.getProvince());
+                jsonObj.put("city", aMapLocation.getCity());
+                jsonObj.put("cityCode", aMapLocation.getCityCode());
+                jsonObj.put("locationType", aMapLocation.getLocationType());
+                jsonObj.put("locationDetail", aMapLocation.getLocationDetail());
+                Log.d("location jsonObj", jsonObj.toString());
                 PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, jsonObj);
-                pluginResult.setKeepCallback(true);
-                callbackContext.sendPluginResult(pluginResult);
+                if (!mLocationOption.isOnceLocation()) {
+                    pluginResult.setKeepCallback(true);
+                } else {
+                    pluginResult.setKeepCallback(false);
+                }
+                cbCtx.sendPluginResult(pluginResult);
             } else {
-                jsonObj.put("errCode", aMapLocation.getAMapException().getErrorCode());
-                jsonObj.put("errMsg", aMapLocation.getAMapException().getMessage());
-                PluginResult pluginResult = new PluginResult(PluginResult.Status.ERROR,jsonObj);
-                pluginResult.setKeepCallback(true);
-                callbackContext.sendPluginResult(pluginResult);
+                jsonObj.put("errCode", aMapLocation.getErrorCode());
+                jsonObj.put("errMsg", aMapLocation.getErrorInfo());
+                PluginResult pluginResult = new PluginResult(PluginResult.Status.ERROR, jsonObj);
+                if (!mLocationOption.isOnceLocation()) {
+                    pluginResult.setKeepCallback(true);
+                } else {
+                    pluginResult.setKeepCallback(false);
+                }
+                cbCtx.sendPluginResult(pluginResult);
             }
         } catch (JSONException e) {
-            callbackContext.error(e.getMessage());
+            cbCtx.error(e.getMessage());
             String errMsg = e.getMessage();
             PluginResult pluginResult = new PluginResult(PluginResult.Status.ERROR, errMsg);
-            callbackContext.sendPluginResult(pluginResult);
             pluginResult.setKeepCallback(false);
+            cbCtx.sendPluginResult(pluginResult);
             stopLocation();
         }
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-
-    }
-
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String s) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
-
     }
 }
--- www/AMapLocationService.js
+++ www/AMapLocationService.js
@@ -1,10 +1,16 @@
 /**
  * Created by lance on 15/10/13.
  */
-var exec = require('cordova/exec');
-var AMapLocationService = function(){};
-AMapLocationService.prototype.execute = function(action,successCallback,errorCallback){
-		exec(successCallback,errorCallback,"AMapLocationService",action,[]);
+ var argscheck = require('cordova/argscheck'),
+	 exec = require('cordova/exec'),
+	AMapLocationService = function(){};
+	AMapLocationService.prototype.locationType = {
+		Hight_Accuracy:0,//高精度
+		Device_Sensors:1,//仅设备(Gps)模式,不支持室内环境的定位
+		Battery_Saving:2//低功耗模式
+	};
+AMapLocationService.prototype.execute = function(action,successCallback,errorCallback,args){
+		exec(successCallback,errorCallback,"AMapLocationService",action,args);
 	};
 	/**
 	*启动定位服务并获取当前定位信息
@@ -12,6 +18,34 @@ AMapLocationService.prototype.execute = function(action,successCallback,errorCal
 AMapLocationService.prototype.getCurrentPosition = function(successCallback, errorCallback){
 	this.execute("getCurrentPosition",successCallback,errorCallback);
 	};
+
+AMapLocationService.prototype.locationWatch = function(successCallback,errorCallback,option){
+	var option = option||{};
+	var getValue = argscheck.getValue;
+	var interval = getValue(option.interval,2);//定位时间间隔 秒
+	var isNeedAddress = getValue(option.isNeedAddress,true);//是否需要地址
+	var isOnceLocation = getValue(option.isOnceLocation,false);//是否单次定位
+	var gpsFirst = getValue(option.gpsFirst,false);//是否优先返回GPS定位信息
+	var httpTimeOut = getValue(option.httpTimeOut,30);//联网超时时间 秒
+	var wifiActiveScan = getValue(option.wifiActiveScan,true);//是否主动刷新wifi
+	var locationType = getValue(option.locationType,0);//定位模式
+	var args = [interval,isNeedAddress,isOnceLocation,gpsFirst,httpTimeOut,wifiActiveScan,locationType];
+	this.execute("watch",successCallback,errorCallback,args);
+};
+
+AMapLocationService.prototype.changeOption = function(successCallback,errorCallback,option){
+	var option = option||{};
+    var getValue = argscheck.getValue;
+    var interval = getValue(option.interval,2);//定位时间间隔 秒
+    var isNeedAddress = getValue(option.isNeedAddress,true);//是否需要地址
+    var isOnceLocation = getValue(option.isOnceLocation,false);//是否单次定位
+    var gpsFirst = getValue(option.gpsFirst,false);//是否优先返回GPS定位信息
+    var httpTimeOut = getValue(option.httpTimeOut,30);//联网超时时间 秒
+    var wifiActiveScan = getValue(option.wifiActiveScan,true);//是否主动刷新wifi
+    var locationType = getValue(option.locationType,0);//定位模式
+    var args = [interval,isNeedAddress,isOnceLocation,gpsFirst,httpTimeOut,wifiActiveScan,locationType];
+    this.execute("changeOption",successCallback,errorCallback,args);
+}
 
 	/**
 	*关闭定位服务
